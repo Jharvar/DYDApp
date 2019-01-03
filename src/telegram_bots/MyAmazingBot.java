17887ea7d38f78122b865434b5addb4c3a09a268
d0ct0r <d0ct0r@parrot>
2019-01-03 21:18:27 +0000

    MarketConcep

--- src/telegram_bots/MyAmazingBot.java
+++ src/telegram_bots/MyAmazingBot.java
@@ -6,40 +6,68 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class MyAmazingBot extends TelegramLongPollingBot {
-
 	@Override
 	public String getBotUsername() {
-		// TODO Auto-generated method stub
-		return "d&d_bot";
+		return "ZaiussBot";
 	}
 
 	@Override
 	public void onUpdateReceived(Update update) {
-		// TODO Auto-generated method stub
-		//Comprobamos si recibimos un mensaje y si el mensaje contiene un texto
 		if(update.hasMessage() && update.getMessage().hasText()) {
-			//creamos una cadena y guardamos el texto recibido
-			String mensajeDeTexto = update.getMessage().getText();
-			//localizamos el identificador del chat  y se lo asignamos a la variable de tipo long
 			long chat_id = update.getMessage().getChatId();
-			//creamos un objeto de tipo SendMessage y le añadimos el id y el texto que se ha enviado
-			SendMessage enviarMensaje = new SendMessage().setChatId(chat_id).setText(mensajeDeTexto);
-			
-			try {
-				//a continuacion ejecutamos el mensaje
-				execute(enviarMensaje);
-			}catch (TelegramApiException telegramException) {
-				// TODO: handle exception
-				telegramException.printStackTrace();
+			System.out.println(chat_id);
+			String mensajeDeTexto = update.getMessage().getText();
+			if(mensajeDeTexto.substring(0,4).compareTo("/ver")==0) {
+				if(mensajeDeTexto.length()==4) {
+					verCategorias(chat_id);
+				}
+				
+				if(mensajeDeTexto.substring(0,10).compareTo("/ver armas")==0) {
+					verArmas(chat_id);
+				}
 			}
-		
 		}
 	}
 
+	public Boolean verCategorias(long chat_id) {
+		String[] categorias = {"armas", "armaduras", "magicos", "clase"};
+		String out = crearMensaje(categorias);
+		SendMessage enviarMensaje = new SendMessage();
+		enviarMensaje.enableHtml(true);
+		enviarMensaje.setChatId(chat_id).setText(out);
+		try {
+			execute(enviarMensaje);
+		}catch (TelegramApiException telegramException) {
+			telegramException.printStackTrace();
+		}
+		return true;
+	}
+	
+	public Boolean verArmas(long chat_id) {
+		String[] armas = {"[001]-Espada larga - 100g", "[002]-Espada Corta - 50g", "[003]-Baston - 100", "[004]-Arco Corto - 50"};
+		String out = crearMensaje(armas);
+		SendMessage enviarMensaje = new SendMessage();
+		enviarMensaje.enableHtml(true);
+		enviarMensaje.setChatId(chat_id).setText(out);
+		try {
+			execute(enviarMensaje);
+		}catch (TelegramApiException telegramException) {
+			telegramException.printStackTrace();
+		}
+		return true;
+	}
+	
+	public String crearMensaje(String[]lista) {
+		String out = "<code>";
+		for (int i = 0; i < lista.length; i++) {
+			out += lista[i];
+			out += "\n";
+		}
+		return out + "</code>";
+	}
+	
 	@Override
 	public String getBotToken() {
-		// TODO Auto-generated method stub
-		return "711497116:AAGrvRTIPFX6hPNtvF0CJeJPfcMKcOjVkto";
+		return "654542961:AAGZDxcg6owBy-P-3oAH8pxQr6ppDfcXe0U";
 	}
-
 }
