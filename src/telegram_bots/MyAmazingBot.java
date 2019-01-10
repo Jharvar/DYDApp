637b2395a56b503aeaa23dd2a65e1d5637645da4
d0ct0r <d0ct0r@parrot>
2019-01-10 00:24:29 +0000

    finish_day09

--- src/telegram_bots/MyAmazingBot.java
+++ src/telegram_bots/MyAmazingBot.java
@@ -25,20 +25,24 @@ public class MyAmazingBot extends TelegramLongPollingBot {
 	@Override
 	public void onUpdateReceived(Update update) {
 		if (update.hasMessage() && update.getMessage().hasText()) {
+			
+			// Guarda chat_id y el input (string)
 			long chat_id = update.getMessage().getChatId();
-			String mensajeDeTexto = update.getMessage().getText();
-			System.out.println(chat_id + " envia: " + mensajeDeTexto);
-
-			// Convierte el input recibido en ArrayList<String> (se valida mejor).
-			ArrayList<String> pArgs = getArgs(mensajeDeTexto);
-
-			// getCommand ya valida, sino devuelve null.
-			String cmd = getCommand(pArgs);
-
+			String userInputString = update.getMessage().getText();
+			System.out.println(chat_id + " envia: " + userInputString);
+			
+			// Tokenizer del string input para enumerar argumentos
+			ArrayList<String> userInput = getInputArray(userInputString);
+			
+			/*
+			 *  Valida si el comando esta disponible,
+			 *  si es un comando registrado entra en el switch.
+			 */
+			String cmd = getCommand(userInput);
 			if (cmd != null) {
 				switch (cmd) {
 				case "/tienda":
-					enviarMensaje(chat_id, CMDtienda(pArgs));
+					CMDtienda(chat_id, userInput);
 					break;
 
 				case "/comprar":
@@ -56,11 +60,12 @@ public class MyAmazingBot extends TelegramLongPollingBot {
 			}
 		}
 	}
-
+	
 	/*
-	 * Devuelve el input en array
+	 * Devuelve un array de strings segun espacios (tokenizer),
+	 * de esta forma se manejan mejor los argumentos.
 	 */
-	public ArrayList<String> getArgs(String msj) {
+	public ArrayList<String> getInputArray(String msj) {
 		StringTokenizer st = new StringTokenizer(msj);
 		ArrayList<String> p = new ArrayList<>();
 		p.clear();
@@ -71,7 +76,7 @@ public class MyAmazingBot extends TelegramLongPollingBot {
 	}
 
 	/*
-	 * Valida el comando y devuelve array valido.
+	 * Valida el comando y devuelve un string con el nombre del comando
 	 */
 	public String getCommand(ArrayList<String> p) {
 		String[] avaliableCommandas = { "/tienda", "/comprar", "/vender" };
@@ -82,31 +87,154 @@ public class MyAmazingBot extends TelegramLongPollingBot {
 		}
 		return null;
 	}
-
+	
 	/*
-	 * Command /tienda
+	 * CMD /tienda
 	 */
-	public String CMDtienda(ArrayList<String> args) {
-		String msj = "";
-		
-		// /tienda (Sin argumento) muestra las categorias
-		if (args.size() == 1) {
-			try {
-				ArrayList<Categorias> categorias = tiendaDB.keepCategories();
-				for (Categorias c : categorias) {
-					msj += c.toStringTiendaHtml();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
+	public void CMDtienda(long chat_id, ArrayList<String> userInput) {
+
+		// /tienda (Sin argumento)
+		if (userInput.size() == 1) {
+			showCategories(chat_id, userInput);
 		}
-		
-		// /tienda arg
-		return msj;
+
+		// /tienda con argumento.
+		else if (userInput.size() == 2) {
+
+			// Argumento de un digito *id_categoria
+			if (userInput.get(1).length() == 1) {
+				showCategory(chat_id, userInput.get(1));
+			}
+
+			// Argumento de 4 digitos *id_categoria(1) + *id_objeto(3)
+			else if (userInput.get(1).length() == 4) {
+				showObject(chat_id, userInput.get(1));
+			}
+
+			// Argumentdo distinto a 1 o 4 (length)
+			else {
+				enviarError(chat_id);
+			}
+		} else {
+			enviarError(chat_id);
+		}
+	}
+	
+	/*
+	 * Muestra listado de categorias.
+	 */
+	public void showCategories(long chat_id, ArrayList<String> userInput) {
+		String msj = "";
+		try {
+			ArrayList<Categorias> categorias = tiendaDB.keepCategories();
+			for (Categorias c : categorias) {
+				msj += c.toStringTiendaHtml();
+			}
+			enviarMensaje(chat_id, msj);
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	/*
-	 * Envia un String a un id
+	 * Muestra lista de objetos de una categoria.
+	 */
+	public void showCategory(long chat_id, String userString) {
+		try {
+			int c = Integer.parseInt(userString);
+			if (isCategory(c)) {
+				// jarvar select category(int)
+				enviarMensaje(chat_id, "Categoria: " + c);
+			} else {
+				enviarError(chat_id);
+			}
+			// else:enviarError(chat_id);
+		} catch (NumberFormatException e) {
+			enviarError(chat_id);
+		}
+	}
+
+	/*
+	 * Muestra un objeto concreto
+	 */
+	private void showObject(long chat_id, String inputCode) {
+		int[] codeArray = new int[2];
+		codeArray[0] = getCodeCategoria(inputCode);
+		codeArray[1] = getCodeObjeto(inputCode);
+		if (codeArray[0] != 0 && codeArray[1] != 0) {
+			enviarMensaje(chat_id, "categoria: " + codeArray[0] + " objeto: " + codeArray[1]);
+		} else {
+			enviarError(chat_id);
+		}
+	}
+
+	/*
+	 * Cuando el argumento es un codigo de objeto (4 caracteres).
+	 * Devuelve int codigo de categoria --> argumento[0]
+	 */
+	public int getCodeCategoria(String inputCode) {
+		// Quizas se pueda mandar cualquier entero y si la bd nos da null
+		// prescindir es estas comprobaciones...
+		int[] catAvaliable = { 1, 2, 3, 4 };
+		try {
+			// Se pilla el int de categoria
+			int c = Integer.parseInt(inputCode.charAt(0) + "");
+
+			// Se comprueba si esta dentro de las categorias disponibles
+			for (int i = 0; i < catAvaliable.length; i++) {
+				if (c == (i + 1)) {
+					return c;
+				}
+			}
+			return 0;
+		} catch (NumberFormatException e) {
+			return 0;
+		}
+	}
+
+	/*
+	 * Cuando el argumento es un codigo de objeto (4 caracteres).
+	 * Devuelve int codigo de objeto --> argumento[1-3]
+	 */
+	public int getCodeObjeto(String inputCode) {
+		if (inputCode.length() == 4) {
+			String codeString = "";
+			try {
+				if (inputCode.charAt(1) == '0' && inputCode.charAt(2) == '0') {
+					codeString += String.valueOf(inputCode.charAt(3));
+					return Integer.parseInt(codeString);
+				} else if (inputCode.charAt(1) == '0') {
+					codeString += String.valueOf(inputCode.charAt(2)) + String.valueOf(inputCode.charAt(3));
+					return Integer.parseInt(codeString);
+				} else {
+					codeString += String.valueOf(String.valueOf(inputCode.charAt(1))
+							+ String.valueOf(inputCode.charAt(2)) + String.valueOf(inputCode.charAt(3)));
+					return Integer.parseInt(codeString);
+				}
+			} catch (NumberFormatException e) {
+				return 0;
+			}
+		}
+		return 0;
+	}
+
+	/*
+	 * Valida si la categoria es valida. 
+	 * (quizas con recibir un null sea suficiente y no haga falta en un futuro)
+	 */
+	public boolean isCategory(int c) {
+		int[] catAvaliable = { 1, 2, 3, 4 };
+		for (int i = 0; i < catAvaliable.length; i++) {
+			if (c == i + 1) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/*
+	 * Enviar un texto
 	 */
 	public Boolean enviarMensaje(long chat_id, String txt) {
 		SendMessage sm = new SendMessage();
